0629d87fd7657117d514d04e328b7ce91216edd7
kingsmurfs <124551711+kingsmurfs@users.noreply.github.com>
2026-04-20 23:06:59 -0300

    Update offs.cpp

--- offs.cpp
+++ offs.cpp
@@ -5,57 +5,57 @@
 namespace Offsets
 {
     // -> offsets.h on a2x dumper (verified from main.cpp working version)
-    constexpr uintptr_t dwEntityList       = 0x21CFEC8;    // Verified working offset
-    constexpr uintptr_t dwLocalPlayerPawn  = 0x206D9E0;    // Verified working offset
-    constexpr uintptr_t dwViewMatrix       = 0x2313F10;    // Verified working offset
+    constexpr uintptr_t dwEntityList       = 0x24C9710;    // Verified working offset
+    constexpr uintptr_t dwLocalPlayerPawn  = 0x204F630;    // Verified working offset
+    constexpr uintptr_t dwViewMatrix       = 0x23290D0;    // Verified working offset
    
     // -> client.dll.hpp on a2x dumper -> C_BaseEntity
-    constexpr uintptr_t m_iHealth          = 0x354;        // 852
-    constexpr uintptr_t m_iTeamNum         = 0x3F3;        // 1011
-    constexpr uintptr_t m_vOldOrigin       = 0x1588;       // 5512
-    constexpr uintptr_t m_pGameSceneNode   = 0x338;        // 824
-    constexpr std::ptrdiff_t m_hPawn              = 0x6C4;     // CBasePlayerController
+    constexpr uintptr_t m_iHealth          = 0x34C;        // 852
+    constexpr uintptr_t m_iTeamNum         = 0x3EB;        // 1011
+    constexpr uintptr_t m_vOldOrigin       = 0x1390;       // 5512
+    constexpr uintptr_t m_pGameSceneNode   = 0x330;        // 824
+    constexpr std::ptrdiff_t m_hPawn              = 0x6BC;     // CBasePlayerController
 
     // -> CbaseModelEntity
-    constexpr uintptr_t m_vecViewOffset    = 0xD58;        // 3416
+    constexpr uintptr_t m_vecViewOffset    = 0xE70;        // 3416
 
     // -> C_CSPlayerPawn
-    constexpr uintptr_t m_iShotsFired      = 0x270C;       // 9996
-    constexpr uintptr_t m_aimPunchAngle    = 0x16CC;       // 5836
-    constexpr uintptr_t m_entitySpottedState = 0x26E0;     // 9952
+    constexpr uintptr_t m_iShotsFired      = 0x1C9C;       // 9996
+    constexpr uintptr_t m_aimPunchAngle    = 0x14D4;       // 5836
+    constexpr uintptr_t m_entitySpottedState = 0x1C70;     // 9952
     
     // -> EntitySpottedState_t
     constexpr uintptr_t m_bSpotted         = 0x8;          // 8
 
     // -> C_CSPlayerController
-    constexpr uintptr_t m_iszPlayerName    = 0x6F8;        // 1784
-    constexpr uintptr_t m_hPlayerPawn      = 0x90C;        // 2316
-    constexpr uintptr_t m_bPawnIsAlive     = 0x914;        // 2324
+    constexpr uintptr_t m_iszPlayerName    = 0x6F0;        // 1784
+    constexpr uintptr_t m_hPlayerPawn      = 0x904;        // 2316
+    constexpr uintptr_t m_bPawnIsAlive     = 0x90C;        // 2324
 
     // -> CGameSceneNode
-    constexpr uintptr_t m_modelState       = 0x160;        // 352
+    constexpr uintptr_t m_modelState       = 0x150;        // 352
 
     // -> New Offsets (Update these for Weapon/Armor indicator to work)
-    constexpr uintptr_t m_ArmorValue       = 0x272C;       // C_CSPlayerPawn -> m_ArmorValue
+    constexpr uintptr_t m_ArmorValue       = 0x1CB4;       // C_CSPlayerPawn -> m_ArmorValue
     constexpr uintptr_t m_pClippingWeapon  = 0x3DC0;       // C_CSPlayerPawn -> m_pClippingWeapon
-    constexpr uintptr_t m_AttributeManager = 0x1378;       // C_EconEntity -> m_AttributeManager (FIXED!)
+    constexpr uintptr_t m_AttributeManager = 0x13B8;       // C_EconEntity -> m_AttributeManager (FIXED!)
     constexpr uintptr_t m_Item             = 0x50;         // C_AttributeContainer -> m_Item
     constexpr uintptr_t m_iItemDefinitionIndex = 0x1BA;    // C_EconItemView -> m_iItemDefinitionIndex
 
     
-    constexpr uintptr_t m_bIsDefusing      = 0x26FA;       // C_CSPlayerPawn -> m_bIsDefusing
-    constexpr uintptr_t m_bIsScoped        = 0x26F8;       // C_CSPlayerPawn -> m_bIsScoped
-    constexpr uintptr_t m_flFlashOverlayAlpha  = 0x15EC;       // C_CSPlayerPawn -> m_flFlashOverlayAlpha
+    constexpr uintptr_t m_bIsDefusing      = 0x1C8A;       // C_CSPlayerPawn -> m_bIsDefusing
+    constexpr uintptr_t m_bIsScoped        = 0x1C88;       // C_CSPlayerPawn -> m_bIsScoped
+    constexpr uintptr_t m_flFlashOverlayAlpha  = 0x13F4;       // C_CSPlayerPawn -> m_flFlashOverlayAlpha
 
-    constexpr uintptr_t m_weaponMode = 0x19A8; // CSWeaponMode
-    constexpr uintptr_t m_flRecoilIndex = 0x19D0; // float32
+    constexpr uintptr_t m_weaponMode = 0x17B8; // CSWeaponMode
+    constexpr uintptr_t m_flRecoilIndex = 0x17E0; // float32
 
-    constexpr uintptr_t m_fFlags = 0x400 // uint32
-    constexpr uintptr_t m_iIDEntIndex = 0x3EAC; // CEntityIndex
-    constexpr uintptr_t m_vecVelocity = 0x438;
+    constexpr uintptr_t m_fFlags = 0x3F8 // uint32
+    constexpr uintptr_t m_iIDEntIndex = 0x341C; // CEntityIndex
+    constexpr uintptr_t m_vecVelocity = 0x430;
 
-    constexpr unitptr_t m_hObserverPawn = 0x910; // CHandle<C_CSObserverPawn>
-    constexpr uintptr_t m_pObserverServices = 0x13F0; // CPlayer_ObserverServices*
+    constexpr unitptr_t m_hObserverPawn = 0x908; // CHandle<C_CSObserverPawn>
+    constexpr uintptr_t m_pObserverServices = 0x11F8; // CPlayer_ObserverServices*
     constexpr uintptr_t m_hObserverTarget = 0x4C; // CHandle<C_BaseEntity>
     constexpr uintptr_t m_iObserverMode = 0x48; // uint8
 
