38559d019f5f3aa506001bb33ae0133c26d07690
kingsmurfs <124551711+kingsmurfs@users.noreply.github.com>
2026-04-23 02:16:32 -0300

    Update offs.cpp

--- offs.cpp
+++ offs.cpp
@@ -20,9 +20,8 @@ namespace Offsets
     constexpr uintptr_t m_vecViewOffset    = 0xE70;        // 3416
 
     // -> C_CSPlayerPawn
-    constexpr uintptr_t m_iShotsFired      = 0x1C9C;       // 9996
-    constexpr uintptr_t m_aimPunchAngle    = 0x14D4;       // 5836
-    constexpr uintptr_t m_entitySpottedState = 0x1C70;     // 9952
+    constexpr uintptr_t m_iShotsFired      = 0x1C5C;       // 9996
+    constexpr uintptr_t m_entitySpottedState = 0x11E0;     // 9952
     
     // -> EntitySpottedState_t
     constexpr uintptr_t m_bSpotted         = 0x8;          // 8
@@ -36,26 +35,26 @@ namespace Offsets
     constexpr uintptr_t m_modelState       = 0x150;        // 352
 
     // -> New Offsets (Update these for Weapon/Armor indicator to work)
-    constexpr uintptr_t m_ArmorValue       = 0x1CB4;       // C_CSPlayerPawn -> m_ArmorValue
-    constexpr uintptr_t m_pClippingWeapon  = 0x3DC0;       // C_CSPlayerPawn -> m_pClippingWeapon
-    constexpr uintptr_t m_AttributeManager = 0x13B8;       // C_EconEntity -> m_AttributeManager (FIXED!)
+    constexpr uintptr_t m_ArmorValue       = 0x1C74;       // C_CSPlayerPawn -> m_ArmorValue
+    constexpr uintptr_t m_AttributeManager = 0x1180;       // C_EconEntity -> m_AttributeManager (FIXED!)
     constexpr uintptr_t m_Item             = 0x50;         // C_AttributeContainer -> m_Item
     constexpr uintptr_t m_iItemDefinitionIndex = 0x1BA;    // C_EconItemView -> m_iItemDefinitionIndex
 
+    constexpr uintptr_t m_hMyWeapons = 0x48; // C_NetworkUtlVectorBase<CHandle<C_BasePlayerWeapon>>
     constexpr uintptr_t m_hActiveWeapon = 0x60; // CHandle<C_BasePlayerWeapon>
     constexpr uintptr_t m_hLastWeapon = 0x64; // CHandle<C_BasePlayerWeapon>
     constexpr uintptr_t m_iAmmo = 0x68; // uint16[32]
     constexpr uintptr_t m_pAimPunchServices  = 0x1490;
     
-    constexpr uintptr_t m_bIsDefusing      = 0x1C8A;       // C_CSPlayerPawn -> m_bIsDefusing
-    constexpr uintptr_t m_bIsScoped        = 0x1C88;       // C_CSPlayerPawn -> m_bIsScoped
+    constexpr uintptr_t m_bIsDefusing      = 0x1C4A;       // C_CSPlayerPawn -> m_bIsDefusing
+    constexpr uintptr_t m_bIsScoped        = 0x1C48;       // C_CSPlayerPawn -> m_bIsScoped
     constexpr uintptr_t m_flFlashOverlayAlpha  = 0x13F4;       // C_CSPlayerPawn -> m_flFlashOverlayAlpha
 
     constexpr uintptr_t m_weaponMode = 0x17B8; // CSWeaponMode
     constexpr uintptr_t m_flRecoilIndex = 0x17E0; // float32
 
     constexpr uintptr_t m_fFlags = 0x3F8 // uint32
-    constexpr uintptr_t m_iIDEntIndex = 0x341C; // CEntityIndex
+    constexpr uintptr_t m_iIDEntIndex = 0x33DC; // CEntityIndex
     constexpr uintptr_t m_vecVelocity = 0x430;
 
     constexpr unitptr_t m_hObserverPawn = 0x908; // CHandle<C_CSObserverPawn>
@@ -63,6 +62,6 @@ namespace Offsets
     constexpr uintptr_t m_hObserverTarget = 0x4C; // CHandle<C_BaseEntity>
     constexpr uintptr_t m_iObserverMode = 0x48; // uint8
 
-    constexpr uintptr_t m_hMyWeapons = 0x48; // C_NetworkUtlVectorBase<CHandle<C_BasePlayerWeapon>>
+   
 }
 
